0712a84d944d0da86a7c74e62ef88d082f2c2c5b
Shivani-kumari <shivanikumari5@yahoo.com>
2022-04-16 21:48:50 +0530

    add action

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
-import {useSelector} from 'react-redux'
+import {useSelector,useDispatch} from 'react-redux'
 
 function App() {
   const counter = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
+  const inc = ()=>{
+    dispatch({type:'INC'})
+  }
+  const dec = ()=>{
+    dispatch({type:'DEC'})
+  }
   return (
     <div className="App">
+      <button onClick={inc}>Increment</button>
+      <button onClick={dec}> Decrement</button>
       {counter}
     </div>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,14 @@
 import {createStore} from 'redux'
 
 const reducerFn = (state = {counter:0},action) =>{
+    // Synchronous Function
+    // We should not mutate the original state
+    if(action.type === "INC"){
+       return {counter : state.counter+1}
+    }
+    if(action.type === "DEC"){
+        return {counter : state.counter-1}
+    }
     return state
 }
 
